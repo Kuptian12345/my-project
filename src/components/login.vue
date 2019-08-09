4d54618b7810c9753c877c50877e71a261c2c6c4
github <Kuptian@163.com>
2019-08-09 16:54:59 +0800

    处理了登录的res

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,68 @@
 <template>
-  <div>login-----</div>
+  <div class="login-wrap">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名称">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button @click.prevent="handleLogin()" class="login-btn" type="success">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    handleLogin() {
+      this.$http
+        .post(`login`, this.formdata)
+        .then(res => {
+          const {
+            data: {
+              data,
+              meta: { msg, status }
+            }
+          } = res;
+          if (status === 200) {
+            this.$message.success("恭喜你,这是一条成功的消息");
+          } else {
+            this.$message.error("错了哦，这是一条错误消息");
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  background-color: #fff;
+  border-radius: 10px;
+  width: 400px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '@/assets/css/base.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+
+Vue.prototype.$http = axios
 
 Vue.use(ElementUI)
 
